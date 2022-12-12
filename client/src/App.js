c4af0176f79c7e3761fcb78aeab3ab45430ff01e
kepha222 <kephamotari1@gmail.com>
2022-12-12 22:35:28 +0300

    projects display on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import Clients from "./components/Clients";
-import AddClientModal from "./components/AddClientModal";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -31,11 +32,15 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
---
+++ client/src/components/Projects.jsx
@@ -0,0 +1,27 @@
+import Spinner from "./Spinner";
+import { useQuery } from "@apollo/client";
+import ProjectCard from "./ProjectCard";
+import { GET_PROJECTS } from "../queries/projectQueries";
+
+const Projects = () => {
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Something Went Wrong!</p>;
+
+  return (
+    <>
+      {data.projects.length > 0 ? (
+        <div className="row mt-4">
+          {data.projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+      ) : (
+        <p>No Projects</p>
+      )}
+    </>
+  );
+};
+
+export default Projects;
